2df4daf05d5dcbf0243a86da928764e0ca10ffc7
riete <loli_520@126.com>
2021-06-07 16:15:06 +0800

    update dingtalk

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/kr/fs v0.1.0 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
 	github.com/pkg/sftp v1.10.0
+	github.com/riete/requests v1.0.2
 	github.com/stretchr/testify v1.3.0 // indirect
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
 )
--- notify/dingtalk.go
+++ notify/dingtalk.go
@@ -1,88 +1,48 @@
 package notify
 
 import (
-	"bytes"
 	"crypto/hmac"
 	"crypto/sha256"
-	"crypto/tls"
 	"encoding/base64"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"net/url"
 	"time"
+
+	"github.com/riete/requests"
 )
 
-func SendDingTalk(title, message, robotUrl string, markdown bool, proxyUrl *url.URL) (string, error) {
-	requestBody := fmt.Sprintf(`{"msgtype": "text","text": {"content": "%s\n\n%s"}}`, title, message)
-	if markdown {
-		requestBody = fmt.Sprintf(
-			`{"msgtype": "markdown","markdown": {"title": "%s", "text": "### %s\n\n%s"}}`, title, title, message,
-		)
+func SendDingTalkMarkdown(title, message, robotUrl, secret string) {
+	var body = map[string]interface{}{
+		"msgtype": "markdown",
+		"markdown": map[string]interface{}{
+			"title": title,
+			"text":  fmt.Sprintf("### %s\n\n%s", title, message),
+		},
 	}
-	jsonStr := []byte(requestBody)
-
-	req, _ := http.NewRequest("POST", robotUrl, bytes.NewBuffer(jsonStr))
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-
-	client := &http.Client{}
-	if proxyUrl != nil {
-		client = &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-				Proxy:           http.ProxyURL(proxyUrl),
-			},
-		}
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	return string(body), nil
+	sendDingTalk(title, message, robotUrl, secret, body)
 }
 
-func SendDingTalkNew(title, message, robotUrl, secret string, markdown bool, proxyUrl *url.URL) (string, error) {
+func SendDingTalkText(title, message, robotUrl, secret string) {
+	var body = map[string]interface{}{
+		"msgtype": "text",
+		"text": map[string]interface{}{
+			"content": fmt.Sprintf("%s\n\n%s", title, message),
+		},
+	}
+	sendDingTalk(title, message, robotUrl, secret, body)
+}
+
+func sendDingTalk(title, message, robotUrl, secret string, body map[string]interface{}) {
 	timestamp := fmt.Sprintf("%d000", time.Now().Unix())
 	sign := fmt.Sprintf("%s\n%s", timestamp, secret)
 	h := hmac.New(sha256.New, []byte(secret))
 	h.Write([]byte(sign))
-
 	signB64 := base64.StdEncoding.EncodeToString([]byte(h.Sum(nil)))
 
 	v := url.Values{}
 	v.Add("sign", signB64)
 	signUrlEncode := v.Encode()
-	postUrl := fmt.Sprintf("%s&timestamp=%s&%s", robotUrl, timestamp, signUrlEncode)
+	sendUrl := fmt.Sprintf("%s&timestamp=%s&%s", robotUrl, timestamp, signUrlEncode)
 
-	requestBody := fmt.Sprintf(`{"msgtype": "text","text": {"content": "%s\n\n%s"}}`, title, message)
-	if markdown {
-		requestBody = fmt.Sprintf(
-			`{"msgtype": "markdown","markdown": {"title": "%s", "text": "### %s\n\n%s"}}`, title, title, message,
-		)
-	}
-	jsonStr := []byte(requestBody)
-
-	req, _ := http.NewRequest("POST", postUrl, bytes.NewBuffer(jsonStr))
-	req.Header.Set("Content-Type", "application/json; charset=utf-8")
-
-	client := &http.Client{}
-	if proxyUrl != nil {
-		client = &http.Client{
-			Transport: &http.Transport{
-				TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-				Proxy:           http.ProxyURL(proxyUrl),
-			},
-		}
-	}
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	return string(body), nil
+	_ = requests.PostJson(sendUrl, body)
 }
