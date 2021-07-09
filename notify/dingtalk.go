0c3c1c0f2dc037f72eb19c1220d683c77d63e0e2
riete <loli_520@126.com>
2021-07-09 13:57:40 +0800

    update dingtalk

--- notify/dingtalk.go
+++ notify/dingtalk.go
@@ -11,28 +11,34 @@ import (
 	"github.com/riete/requests"
 )
 
-func SendDingTalkMarkdown(title, message, robotUrl, secret string) {
+func SendDingTalkMarkdown(title, message, robotUrl, secret string, isAtAll bool) string {
 	var body = map[string]interface{}{
 		"msgtype": "markdown",
 		"markdown": map[string]interface{}{
 			"title": title,
 			"text":  fmt.Sprintf("### %s\n\n%s", title, message),
 		},
+		"at": map[string]interface{}{
+			"isAtAll": isAtAll,
+		},
 	}
-	sendDingTalk(title, message, robotUrl, secret, body)
+	return SendDingTalk(title, message, robotUrl, secret, body)
 }
 
-func SendDingTalkText(title, message, robotUrl, secret string) {
+func SendDingTalkText(title, message, robotUrl, secret string, isAtAll bool) string {
 	var body = map[string]interface{}{
 		"msgtype": "text",
 		"text": map[string]interface{}{
 			"content": fmt.Sprintf("%s\n\n%s", title, message),
 		},
+		"at": map[string]interface{}{
+			"isAtAll": isAtAll,
+		},
 	}
-	sendDingTalk(title, message, robotUrl, secret, body)
+	return SendDingTalk(title, message, robotUrl, secret, body)
 }
 
-func sendDingTalk(title, message, robotUrl, secret string, body map[string]interface{}) {
+func SendDingTalk(title, message, robotUrl, secret string, body map[string]interface{}) string {
 	timestamp := fmt.Sprintf("%d000", time.Now().Unix())
 	sign := fmt.Sprintf("%s\n%s", timestamp, secret)
 	h := hmac.New(sha256.New, []byte(secret))
@@ -44,5 +50,10 @@ func sendDingTalk(title, message, robotUrl, secret string, body map[string]inter
 	signUrlEncode := v.Encode()
 	sendUrl := fmt.Sprintf("%s&timestamp=%s&%s", robotUrl, timestamp, signUrlEncode)
 
-	_ = requests.PostJson(sendUrl, body)
+	r := requests.NewRequest()
+	if err := r.PostJson(sendUrl, body); err != nil {
+		return err.Error()
+	} else {
+		return r.Content
+	}
 }
